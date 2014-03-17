a6734386c1f7a922c05b5d490c2c5f66d3af7900
opendroid-Team <opendroid2014@gmail.com>
2014-03-17 22:20:38 +0100

    add recipes-distros and opendroid.conf (Test)

--- recipes-distros/opendroid/bootlogo/files/bootlogo.sh
+++ recipes-distros/opendroid/bootlogo/files/bootlogo.sh
@@ -1,4 +1,15 @@
-# thoug not actually a bootlogo task, set the correct videomode before showing the bootlogo
-cat /etc/videomode > /proc/stb/video/videomode
+# avoid the console messages clobbering our logo
+[ -f /sys/class/vtconsole/vtcon1/bind ] && echo 0 > /sys/class/vtconsole/vtcon1/bind
+# and set the correct videomode before showing the bootlogo
+[ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
-[ -e /etc/dropbear/dropbear_rsa_host_key ] &&  /usr/bin/showiframe /usr/share/bootlogo.mvi || /usr/bin/showiframe /usr/share/bootlogo_wait.mvi
+BOOTLOGO=/usr/share/bootlogo.mvi
+BOOTLOGO1=/usr/share/bootlogo1.mvi	
+	if [ -e /etc/.e2 ]; then
+		[ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
+		/usr/bin/showiframe ${BOOTLOGO}
+	elif [ -e /etc/.nhd2 ]; then
+		[ -f /etc/enigma2/bootlogo1.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo1.mvi
+		/usr/bin/showiframe ${BOOTLOGO1}
+	fi
+
